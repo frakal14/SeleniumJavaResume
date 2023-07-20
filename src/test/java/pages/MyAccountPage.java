8ae28bd1217d6c8ab219e39a03f9b0e4b961d1af
Frakal14 <karol.fraczkowski@gmail.com>
2023-07-20 14:16:25 +0200

    Added MyAccountPage

---
+++ src/test/java/pages/MyAccountPage.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class MyAccountPage extends BasePage {
+
+    public MyAccountPage(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+
+
+
+
+
+}
